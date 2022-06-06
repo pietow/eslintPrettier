adf936a922a138e9fd9ab5de55a92fa78535edb3
piet <pprotassow@posteo.de>
2022-06-07 00:54:23 +0200

    feat: eslint + prettier

---
+++ app.js
@@ -0,0 +1,31 @@
+/** @format */
+
+const express = require('express')
+
+const handlers = require('./lib/handlers')
+
+var b = 0
+console.log(bla)
+
+const port = process.env.PORT || 3033
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.post('/api/login', (req, res) => {
+    if (req.body.user === 'admin' && req.body.password === 'admin') {
+        res.send({
+            token: 'token123',
+        })
+    } else {
+        res.send({ token: 'nope' })
+    }
+})
+/* app.post("/api/login", handlers.postLogin); */
+
+app.listen(port, () => {
+    console.log(
+        `Express started on http://localhost:${port}` +
+            '; press Ctrl-C to terminate.',
+    )
+})
